d00193aaa9e1dbd81d5a46317737ad02977f025c
Francisco Javier Moreno <frajamomo@hotmail.com>
2015-12-18 10:49:52 +0000

    Changed filename menu prompt in vimrc

---
+++ speedtest.sh
@@ -0,0 +1,5 @@
+echo "Testing with a 10 Mb file download from internet"
+wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip
+
+echo "Testing with a 100 Mb file download from internet"
+wget -O /dev/null http://speedtest.wdc01.softlayer.com/downloads/test100.zip
