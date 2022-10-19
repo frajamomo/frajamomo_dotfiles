48846126ddcc694ffb56479f27cafbd51331530e
Francisco Javier Moreno <frajamomo@hotmail.com>
2022-10-19 12:44:26 +0200

    fix Non-ASCII characters

--- Singleton.py
+++ Singleton.py
@@ -1,18 +1,12 @@
 """
-El patrón de diseño singleton (instancia única) está diseñado para restringir la creación de objetos pertenecientes a una clase o el valor de un tipo a un único objeto.
-
-Su intención consiste en garantizar que una clase sólo tenga una instancia y proporcionar un punto de acceso global a ella.
-
-El patrón singleton se implementa creando en nuestra clase un método que crea una instancia del objeto sólo si todavía no existe alguna. Para asegurar que la clase no puede ser instanciada nuevamente se regula el alcance del constructor (con atributos como protegido o privado).
-
-La instrumentación del patrón puede ser delicada en programas con múltiples hilos de ejecución. Si dos hilos de ejecución intentan crear la instancia al mismo tiempo y esta no existe todavía, sólo uno de ellos debe lograr crear el objeto. La solución clásica para este problema es utilizar exclusión mutua en el método de creación de la clase que implementa el patrón.
-
-Las situaciones más habituales de aplicación de este patrón son aquellas en las que dicha clase controla el acceso a un recurso físico único (como puede ser el ratón o un archivo abierto en modo exclusivo) o cuando cierto tipo de datos debe estar disponible para todos los demás objetos de la aplicación.
-
-El patrón singleton provee una única instancia global gracias a que:
-
-    La propia clase es responsable de crear la única instancia.
-    Permite el acceso global a dicha instancia mediante un método de clase.
+El patron de diseno singleton (instancia unica) esta disenado para restringir la creacion de objetos pertenecientes a una clase o el valor de un tipo a un unico objeto.
+Su intencion consiste en garantizar que una clase solo tenga una instancia y proporcionar un punto de acceso global a ella.
+El patron singleton se implementa creando en nuestra clase un metodo que crea una instancia del objeto solo si todavia no existe alguna. Para asegurar que la clase no puede ser instanciada nuevamente se regula el alcance del constructor (con atributos como protegido o privado).
+La instrumentacion del patron puede ser delicada en programas con multiples hilos de ejecucion. Si dos hilos de ejecucion intentan crear la instancia al mismo tiempo y esta no existe todavia, solo uno de ellos debe lograr crear el objeto. La solucion clasica para este problema es utilizar exclusion mutua en el metodo de creacion de la clase que implementa el patron.
+Las situaciones mas habituales de aplicacion de este patron son aquellas en las que dicha clase controla el acceso a un recurso fisico unico (como puede ser el raton o un archivo abierto en modo exclusivo) o cuando cierto tipo de datos debe estar disponible para todos los demas objetos de la aplicacion.
+El patron singleton provee una unica instancia global gracias a que:
+    La propia clase es responsable de crear la unica instancia.
+    Permite el acceso global a dicha instancia mediante un metodo de clase.
     Declara el constructor de clase como privado para que no sea instanciable directamente.
 """
 class Singleton (object):
